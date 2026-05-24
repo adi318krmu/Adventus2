00950dbcb62f752a33f613a40116a6b0bb6beeec
@A.D. <157805870+adi318krmu@users.noreply.github.com>
2026-05-24 12:57:23 +0530

    fixing cors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,26 @@ const __dirname = path.dirname(__filename);
 connectDB();
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+app.use(cors({
+ origin:function(origin,callback){
+   const allowedOrigins=[
+     "http://localhost:5173",
+     "https://adventus2.vercel.app"
+   ];
+
+   if(
+     !origin ||
+     allowedOrigins.includes(origin) ||
+     origin.endsWith(".vercel.app")
+   ){
+      callback(null,true);
+   } else {
+      callback(new Error("Not allowed by CORS"));
+   }
+ },
+ credentials:true
+}));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
